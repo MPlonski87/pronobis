6f7474088d29d4a14daba4dd2854010e97c7fdf0
Michael Plonski <michael@nextindex.de>
2018-01-29 16:11:39 +0100

    Remove unneeded blank

--- wp-content/themes/pronobis/gulpfile.js
+++ wp-content/themes/pronobis/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass  = require('gulp-sass');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
