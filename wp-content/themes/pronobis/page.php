e97f10a216d6915d3b0311c542958870b4164f43
Michael Plonski <michael@nextindex.de>
2018-01-29 17:39:35 +0100

    Edit column template

--- wp-content/themes/pronobis/page.php
+++ wp-content/themes/pronobis/page.php
@@ -17,8 +17,10 @@
 			<div class="block__element animated fadeIn">Lorem ipsum dolor sit amet.</div>
 
 			<div class="row">
-				<div class="col-xs-12 col-md-6">Column 1</div>
-				<div class="col-xs-12 col-md-6">Column 2</div>
+				<div class="col-xs-12 col-md-3">Column 1</div>
+				<div class="col-xs-12 col-md-3">Column 2</div>
+				<div class="col-xs-12 col-md-3">Column 3</div>
+				<div class="col-xs-12 col-md-3">Column 4</div>
 			</div>
 		</main>
 
