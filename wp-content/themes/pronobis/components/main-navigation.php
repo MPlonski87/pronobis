c4057095ee873452429503083c02484f6f9270a2
MPlonski87 <kontakt@michaelplonski.de>
2018-04-17 19:25:23 +0200

    Add components

---
+++ wp-content/themes/pronobis/components/main-navigation.php
@@ -0,0 +1,22 @@
+<div class="main-navigation">
+	<div class="row middle-md">
+		<div class="col-md-3">
+			<a class="main-navigation__logo" href="/pronobis/">
+				<img src="<?php echo get_template_directory_uri() . 
+					'/assets/images/main-navigation/logo.png'; ?>" alt="Pronobis Demenzbetreuung Logo">
+			</a>
+		</div>
+		<div class="col-md-3 col-md-offset-6">
+			<div class="main-navigation__links">
+				<a href="tel:0201409811">
+					<img src="<?php echo get_template_directory_uri() . 
+						'/assets/images/main-navigation/button-telefon.jpg'; ?>" alt="Button Telefon">
+				</a>						
+				<a href="/pronobis/kontakt/">
+					<img src="<?php echo get_template_directory_uri() . 
+						'/assets/images/main-navigation/button-kontakt.jpg'; ?>" alt="Button Kontakt">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ wp-content/themes/pronobis/components/site-navigation.php
@@ -0,0 +1,3 @@
+<div class="site-navigation">
+	<?php wp_nav_menu( array('theme_location' => 'header-menu')); ?>
+</div>
