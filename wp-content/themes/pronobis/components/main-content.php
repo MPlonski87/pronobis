6f9493640dc1598d23b41f79f677a54f81edbcaa
MPlonski87 <kontakt@michaelplonski.de>
2018-04-20 18:41:18 +0200

    Fix IE bugs

--- wp-content/themes/pronobis/components/main-content.php
+++ wp-content/themes/pronobis/components/main-content.php
@@ -1,16 +1,16 @@
-<main class="main-content">
-	<div class="row">
-		<div class="col-xs-12 col-md-3">
-			<?php the_field('col-1'); ?>
-		</div>		
-		<div class="col-xs-12 col-md-3">
-			<?php the_field('col-2'); ?>
-		</div>		
-		<div class="col-xs-12 col-md-3">
-			<?php the_field('col-3'); ?>
-		</div>		
-		<div class="col-xs-12 col-md-3">
-			<?php the_field('col-4'); ?>
-		</div>		
-	</div>
-</main>
+	<main class="main-content">
+		<div class="row">
+			<div class="col-xs-12 col-md-3">				
+				<?php the_field('col-1'); ?>
+			</div>		
+			<div class="col-xs-12 col-md-3">
+				<?php the_field('col-2'); ?>
+			</div>		
+			<div class="col-xs-12 col-md-3">
+				<?php the_field('col-3'); ?>
+			</div>		
+			<div class="col-xs-12 col-md-3">
+				<?php the_field('col-4'); ?>
+			</div>		
+		</div>
+	</main>
