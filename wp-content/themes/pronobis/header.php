813d6f4fbe2552e42935ff38c2eaf63018dc4d35
MPlonski87 <kontakt@michaelplonski.de>
2018-04-20 18:41:06 +0200

    Add meta tags

--- wp-content/themes/pronobis/header.php
+++ wp-content/themes/pronobis/header.php
@@ -2,10 +2,26 @@
 <html lang="de">
 
 	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117971068-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-117971068-1');
+		</script>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="author" content="Michael Plonski">
+		<meta name="author" content="Dorothee Hammer">
+		<meta name="publisher" content="Michael Plonski">
+		<meta name="copyright" content="Michael Plonski und Norbert Hammer">
+		<meta name="description" content="Pronobis Demenzbetreuung Dorothee Hammer">
+		<meta name="keywords" content="Pronobis, Demenzbetreuung, Dorothee, Hammer">
+		<meta name="page-topic" content="Dienstleistung">
+		<meta name="audience" content="Erwachsene"><meta http-equiv="content-language" content="de">
+		<meta name="robots" content="index, follow">		
 		<title>Pronobis Demenzbetreuung | Dorothee Hammer</title>
 		<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
 		<link rel="icon" href="<?php echo get_stylesheet_directory_uri() . '/favicon.ico'; ?>" type="image/x-icon" />
