ea6df20480378a848cfc8970eff8207881697ea9
MPlonski87 <kontakt@michaelplonski.de>
2018-04-23 17:42:53 +0200

    Add alt text to image

--- wp-content/themes/pronobis/components/scroll-to-top.php
+++ wp-content/themes/pronobis/components/scroll-to-top.php
@@ -1,1 +1,1 @@
-	<a href="#" class="back-to-top"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/scroll-to-top/scroll-to-top.png'; ?>" alt=""></a>
+	<a href="#" class="back-to-top"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/scroll-to-top/scroll-to-top.png'; ?>" alt="Scroll to top Button"></a>
