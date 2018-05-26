35e5b754a49857068bfb1b41974449dcfce146dc
MPlonski87 <kontakt@michaelplonski.de>
2018-05-26 16:43:34 +0200

    Add alt tags to 404 template

--- wp-content/themes/pronobis/404.php
+++ wp-content/themes/pronobis/404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
       <div class="hero-image animated fadeIn">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/hero-image/impressum/hero-impressum.jpg'; ?>" alt="Vorschaubild Impressum">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/hero-image/impressum/hero-impressum.jpg'; ?>" alt="Vorschaubild 404">
       </div>
     <?php include (get_template_directory() . '/components/site-navigation.php'); ?>        
   </header>
