69362af8bef8e7be3e96d76a3b88ee7d4e3e6753
MPlonski87 <kontakt@michaelplonski.de>
2018-05-26 16:43:19 +0200

    Add alt tags to page template

--- wp-content/themes/pronobis/page.php
+++ wp-content/themes/pronobis/page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 	    <div class="hero-image hero-image--sm animated fadeIn">
 				<?php if (get_field('hero-image-lg')): ?>
-					<img src="<?php the_field('hero-image-lg'); ?>" />
+					<img src="<?php the_field('hero-image-lg'); ?>" alt="Vorschaubild Startseite"/>
 				<?php endif; ?>        
 	    </div>
 	    <div class="hero-image hero-image--xs animated fadeIn">
 				<?php if (get_field('hero-image-xs')): ?>
-					<img src="<?php the_field('hero-image-xs'); ?>" />
+					<img src="<?php the_field('hero-image-xs'); ?>" alt="Vorschaubild Startseite"/>
 				<?php endif; ?>  
 	    </div>	    
 		<?php include (get_template_directory() . '/components/site-navigation.php'); ?>	    
